d15a228ec4f753008960ce60d5a5c7f20ed7041b
Igor de Oliveira Barros Faluhelyi <u00378@brbseguros.com.br>
2023-04-03 10:58:58 -0300

    EDA deprecated for large memory usage

---
+++ scripts/EDA_2.R
@@ -0,0 +1,23 @@
+#########################
+### Working directory ###
+#########################
+library(glue)
+tryCatch(stop(setwd("C:/Users/igor/Desktop/TCC_EST_UNB")),
+         error=function(e) print('Voce esta na brb seguros'),
+         finally=setwd("C:/Users/u00378/Desktop/TCC_EST_UNB"))
+
+########################
+### Import .pkl file ###
+########################
+#install.packages('reticulate')
+require("reticulate")
+
+PATH = glue("{getwd()}")
+source_python(glue("{PATH}/scripts/pickle_reader.py"))
+pickle_data <- read_pickle_file(glue("{PATH}/dados_tcc.pkl"))
+
+###########
+### EDA ###
+###########
+
+# DEPRECATED FOR LARGE MEMORY USAGE!!!
